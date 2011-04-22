27bb19bff969f7ae7e669d7970a3c617c6c6e817
Brendan Ragan <lordmortis@gmail.com>
2011-04-22 08:51:58 +0800

    Need to ensure that if the devise credentials don't exist, the site still boots!

--- config/initializers/devise_credentials.rb
+++ config/initializers/devise_credentials.rb
@@ -1,6 +1,8 @@
 Devise.setup do |config|
-	DEVISE_CREDENTIALS = YAML.load_file("#{RAILS_ROOT}/privateconfig/devise_credentials.yml")
-	DEVISE_CREDENTIALS.keys.each do |authtype|
-		config.omniauth authtype.parameterize.underscore.to_sym, DEVISE_CREDENTIALS[authtype]['app_id'], DEVISE_CREDENTIALS[authtype]['app_secret']
+	if FileTest.exists?("#{RAILS_ROOT}/privateconfig/devise_credentials.yml")
+		DEVISE_CREDENTIALS = YAML.load_file("#{RAILS_ROOT}/privateconfig/devise_credentials.yml")
+		DEVISE_CREDENTIALS.keys.each do |authtype|
+			config.omniauth authtype.parameterize.underscore.to_sym, DEVISE_CREDENTIALS[authtype]['app_id'], DEVISE_CREDENTIALS[authtype]['app_secret']
+		end
 	end
 end
