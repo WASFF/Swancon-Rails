a31d3cded507a6612edfd03e1a1e3bd393fa29be
Brendan Ragan <lordmortis@gmail.com>
2011-05-01 22:05:23 +0800

    New theme!

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,10 +2,6 @@ class IndexController < ApplicationController
 	filter_access_to :all
 
 	def index
-		# need to direct to index content!
-	end
-	
-	def content_admin
-	end
-	
+		redirect_to :controller => "content_viewer", :action => "page"
+	end	
 end
