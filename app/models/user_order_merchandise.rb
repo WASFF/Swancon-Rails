5b309c6cb9fd957ab18afee1f5e5a200c73c91ff
Brendan Ragan <lordmortis@gmail.com>
2011-05-04 00:23:01 +0800

    Order lists now sort by types so everything is grouped.

--- app/models/user_order_merchandise.rb
+++ app/models/user_order_merchandise.rb
@@ -25,9 +25,40 @@ class UserOrderMerchandise < ActiveRecord::Base
 		joins(:user_order).where(UserOrder.paidarel).where("shipped_at IS NOT NULL")
 	}
 	
-	scope :optionsort, lambda {
-		
+	scope :types, lambda {
+		group(:merchandise_type_id)
 	}
+	
+	def self.sorted_option_list(collection, type)
+		collectionids = collection.select("user_order_merchandises.id").all.collect {|item| item.id}
+		if collectionids.length == 0
+			return where("1 = 1")
+		end
+		optionids = collection.joins(:user_order_merchandise_options).group(:merchandise_option_id).select(:merchandise_option_id).collect {|item| item.merchandise_option_id}
+		setids = MerchandiseOption.where(:id => optionids).select(:merchandise_option_set_id).group(:merchandise_option_set_id).collect{|item| item.merchandise_option_set_id}
+			
+		selectfrag = ""
+		orderfrag = ""
+		setids.each do |id|
+			selectfrag += ", (
+			SELECT user_order_merchandise_options.merchandise_option_id
+			FROM user_order_merchandise_options
+			JOIN merchandise_options ON merchandise_options.id = user_order_merchandise_options.merchandise_option_id
+			WHERE user_order_merchandise_options.user_order_merchandise_id = user_order_merchandises.id AND merchandise_options.merchandise_option_set_id = #{id}
+			) as set_#{id}"
+			orderfrag += "set_#{id}, "
+		end
+
+		collectionfrag = "("
+		collectionids.each do |id|
+			collectionfrag += "#{id}, "
+		end
+		collectionfrag = "#{collectionfrag[0..-3]})"
+		
+		sql = "SELECT user_order_merchandises.*#{selectfrag} FROM user_order_merchandises WHERE user_order_merchandises.merchandise_type_id = #{type} AND id IN #{collectionfrag} ORDER BY #{orderfrag[0..-3]}"
+		
+		self.find_by_sql(sql)
+	end
 		
 	def price
 		merchandise_type.price
