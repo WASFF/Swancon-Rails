8e0d1b11fd4c8df7f3da103229674d2931dff9ca
Brendan Ragan <lordmortis@gmail.com>
2012-03-04 16:58:40 +0800

    Cross-post to swan con mailing list.

--- config/initializers/mortiscms.rb
+++ config/initializers/mortiscms.rb
@@ -5,9 +5,10 @@ Mortiscms.config do |config|
 	config.show_titles_in_tags = true
 	config.routes_enabled = true
 	config.publish_to_email = {query: MemberDetail.email_list, name: :name_real, email: :email}
-	config.publish_from_address = "donotreply@swancon.com.au"
+	config.publish_from_address = "Doom-Con, Swancon 2012 <donotreply@swancon.com.au>"
 	config.link_root = "http://2012.swancon.com.au/"
 	config.writer_query_message = :is_writer?
 	config.editor_query_message = :is_editor?
 	config.user_model = "User"
+	config.publish_extra_emails = [["Swancon", "swancon@guild.uwa.edu.au"]]
 end
