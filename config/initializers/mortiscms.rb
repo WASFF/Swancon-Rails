e93e6e2d383d1c01369731dd9fd491a705d9e289
Brendan Ragan <lordmortis@gmail.com>
2012-03-13 13:17:33 +0800

    Upgraded Dependencies.

--- config/initializers/mortiscms.rb
+++ config/initializers/mortiscms.rb
@@ -5,7 +5,8 @@ Mortiscms.config do |config|
 	config.show_titles_in_tags = true
 	config.routes_enabled = true
 	config.publish_to_email = {query: MemberDetail.email_list, name: :name_real, email: :email}
-	config.publish_from_address = "Doom-Con, Swancon 2012 <donotreply@swancon.com.au>"
+	config.publish_from_address = "donotreply@swancon.com.au"
+	config.publish_from_friendly_address = "Doom-Con: Swancon 2012 Auto Mailer <donotreply@swancon.com.au>"
 	config.link_root = "http://2012.swancon.com.au/"
 	config.writer_query_message = :is_writer?
 	config.editor_query_message = :is_editor?
