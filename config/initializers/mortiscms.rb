1ae5e267c0ba4f64608d8940be32a63ea3e08f96
Brendan Ragan <lordmortis@gmail.com>
2012-04-08 15:04:45 +0800

    Year number for emails and ticket printing. Ensure that User ID is displayed prominently

--- config/initializers/mortiscms.rb
+++ config/initializers/mortiscms.rb
@@ -1,14 +1,14 @@
 Mortiscms.config do |config|
 	config.content_admin_route = {:controller => "admin", :action => "content"}
-	config.site_name = "DoomCon 2012"
+	config.site_name = "Swancon 2013"
 	config.show_titles_in_pages = true
 	config.show_titles_in_tags = true
 	config.routes_enabled = true
 	#config.publish_to_email = {query: MemberDetail.email_list, name: :name_real, email: :email}
 	config.publish_to_email = false
 	config.publish_from_address = "donotreply@swancon.com.au"
-	config.publish_from_friendly_address = "Doom-Con: Swancon 2012 Auto Mailer <donotreply@swancon.com.au>"
-	config.link_root = "http://2012.swancon.com.au/"
+	config.publish_from_friendly_address = "Swancon 2013 Auto Mailer <donotreply@swancon.com.au>"
+	config.link_root = "http://2013.swancon.com.au/"
 	config.writer_query_message = :is_writer?
 	config.editor_query_message = :is_editor?
 	config.user_model = "User"
