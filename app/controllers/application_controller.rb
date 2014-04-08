3206ab86e832852eb4e796f20f26e3804f23442a
Brendan Ragan <lordmortis@gmail.com>
2014-04-08 22:29:43 +0800

    temporary fix for after-sign-in to make it work better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,10 +39,10 @@ class ApplicationController < ActionController::Base
 	end
 
 	def after_sign_in_path_for(resource_or_scope)
-		if permitted_to? :index, :seller
-			{action: :sales, controller: "/seller"}
-		else
+#		if permitted_to? :index, :seller
+#			{action: :sales, controller: "/seller"}
+#		else
 			root_path
-		end
+#		end
 	end
 end
