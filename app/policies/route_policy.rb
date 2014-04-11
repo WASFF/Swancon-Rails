2c28eaeaeff3bcf4ce46cb08837e96748553262f
Brendan Ragan <lordmortis@gmail.com>
2014-04-11 12:02:47 +0800

    build up route policy

--- app/policies/route_policy.rb
+++ app/policies/route_policy.rb
@@ -2,9 +2,27 @@ class RoutePolicy
   attr_reader :user, :path
 
   ROLE_PATHS = {
-    admin: {index: true},
-    committee: {index: true},
-    user: {tickets: [:transfer, :find_user, :confirm_transfer, :reconfirm_transfer, :my ]},
+    admin: {
+      index: true,
+      tickets: true,
+      user_orders: true,
+      seller: true
+    },
+    committee: {
+      index: true,
+      tickets: [:index, :export_badges, :export_salesdata, :card_issue, :card_unissue ],
+      user_orders: :remail,
+      seller: true
+    },
+    ticket_seller: {
+      user_orders: :show,
+      seller: true,
+      member_details: [:new, :create]
+    },
+    user: {
+      tickets: [:transfer, :find_user, :confirm_transfer, :reconfirm_transfer, :my ],
+      user_orders: [:index, :show, :void, :unvoid ]
+    },
     guest: {index: [:index]}
   }.freeze
 
@@ -14,11 +32,15 @@ class RoutePolicy
 
   def visit?(controller, action)
     allowed_paths = {}
+    merged_user = false
     unless @user.blank?
       @user.role_symbols.each do |role|
         allowed_paths = merge_paths(allowed_paths, ROLE_PATHS[role]) if ROLE_PATHS.has_key? role
+        if role == :user
+          merged_user = true
+        end
       end
-      allowed_paths = merge_paths(allowed_paths, ROLE_PATHS[:user])
+      allowed_paths = merge_paths(allowed_paths, ROLE_PATHS[:user]) unless merged_user
     end
     allowed_paths = merge_paths(allowed_paths, ROLE_PATHS[:guest])
     controller = controller.to_sym
@@ -40,11 +62,15 @@ private
         if merged_val_class != TrueClass
           merged_val = merged[key] 
           merged_val = [merged[key]] unless merged_val_class == Array
-          if val.class == Array
+          val_class = val.class
+          if val_class == TrueClass
+            merged_val = true
+          elsif val_class == Array
             merged_val += val
           else
             merged_val += [val]
           end
+          merged[key] = merged_val
         end
       end
     end
