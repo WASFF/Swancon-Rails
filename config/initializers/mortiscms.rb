0511ca31c4c21e67868479567494b992875626c2
Brendan Ragan <lordmortis@gmail.com>
2014-04-11 14:26:54 +0800

    remove unused configuration directive

--- config/initializers/mortiscms.rb
+++ config/initializers/mortiscms.rb
@@ -7,7 +7,6 @@ Mortiscms.config do |config|
 	config.publish_to_email = {query: MemberDetail.email_list, name: :name_real, email: :email}
 	config.publish_from_address = "donotreply@swancon.com.au"
 	config.publish_from_friendly_address = "Swancon 2014 Auto Mailer <donotreply@swancon.com.au>"
-	config.link_root = "http://2014.swancon.com.au/"
 	config.authorization_system = :pundit
 	config.user_model = "User"
 	config.publish_extra_emails = [["Swancon", "swancon@guild.uwa.edu.au"]]
