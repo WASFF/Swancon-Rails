7e19e0fc43c82f0a35dadc959f7e8221e596bec3
Brendan Ragan <lordmortis@gmail.com>
2014-04-11 22:31:17 +0800

    Revert "symlink public assets for faster deploys"
    This reverts commit 019d1c204eae3378da96de7379a2c9e42c735155.

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :rvm_ruby_version, '2.1.1'
 set :linked_files, %w{config/database.yml config/smtp.yml db/staging.sqlite3}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
