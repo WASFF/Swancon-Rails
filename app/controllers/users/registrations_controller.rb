dfe19272401eecd3da9c3f3e8bfa117e9fcae3b8
Brendan Ragan <lordmortis@gmail.com>
2015-02-06 13:39:00 +0800

    move registrations around and prep for modification to password recovery

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,4 @@
-class RegistrationsController < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :update_sanitized_params, if: :devise_controller?
 
   def update
