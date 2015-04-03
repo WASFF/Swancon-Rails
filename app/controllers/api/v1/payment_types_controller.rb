8fb0ee5b5c2948d2da6fc17a7bc44d06c59a7cf8
Brendan Ragan <lordmortis@gmail.com>
2015-04-03 11:57:45 +0800

    Ensure that not-in-con-mode site can sell cash / eftpos tickets

--- app/controllers/api/v1/payment_types_controller.rb
+++ app/controllers/api/v1/payment_types_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::PaymentTypesController < ApplicationController
     if SiteSettings.con_mode
       respond_with PaymentType.con_types
     else
-      respond_with PaymentType.online_types
+      respond_with PaymentType.reseller_types
     end
   end
 end
