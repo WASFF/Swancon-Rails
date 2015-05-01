99e613c22d2c85f036ca5db86716f7e970c3a0a4
Brendan Ragan <lordmortis@gmail.com>
2015-05-01 13:43:48 +0800

    any future 2015 deploys should be from sc2015 branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ role :db,  %w{sc2015@swancon-mel1.swancon.com.au}
 
 set :deploy_to, '/home/sc2014/SwanconRails/production'
 
-set :branch, :master
+set :branch, "sc2015"
 
 set :deploy_to, '/home/sc2015/SwanconRails/production'
 set :linked_files, fetch(:base_linked_files) + ["db/production.sqlite3"]
