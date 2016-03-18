9fe8a46e83f863c4126c2efa151b9e94ebd655eb
Brendan Ragan <lordmortis@gmail.com>
2016-03-18 20:00:28 +0800

    blat user data to a file

---
+++ app/exporters/user_data_exporter.rb
@@ -0,0 +1,40 @@
+class UserDataExporter
+
+  def self.export(user_record)
+    data = Hash.new
+    [:id, :username, :email, :password].each do |attribute|
+      value = user_record.send(attribute)
+      data[attribute] = value
+    end
+    [:confirmed_at, :confirmation_sent_at, :created_at, :updated_at].each do |attribute|
+      value = user_record.send(attribute)
+      data[attribute] = user_record.send(attribute).iso8601 if value.present?
+    end
+
+    if user_record.member_detail
+      [
+        :name_first, :name_last, :name_badge, :address_1, :address_2, :address_3,
+        :address_postcode, :address_state, :address_country, :phone,
+        :email_optin, :disclaimer_signed
+      ].each do |attribute|
+        value = user_record.member_detail.send(attribute)
+        if value.is_a? String
+          value = value.force_encoding(Encoding::UTF_8)
+        end
+        data[attribute] = value
+      end
+
+      [:created_at, :updated_at].each do |attribute|
+        value = user_record.member_detail.send(attribute)
+        data[attribute] = user_record.member_detail.send(attribute).iso8601 if value.present?
+      end
+    end
+    data
+  end
+
+  def self.export_all
+    User.all.collect do |user|
+      self.export(user)
+    end
+  end
+end
--- lib/tasks/config_admin_user.rake
+++ lib/tasks/config_admin_user.rake
@@ -9,24 +9,24 @@ namespace :swancon do
 			STDOUT.flush
 			password = STDIN.gets.chomp!
 			break if gotpass
-		
+
 			if (password.empty?)
 				password = `apg -M NC -n 1`.chomp!
 			end
-		
+
 			print "Password will be: '#{password}'\n"
 			print "Okay? (y/n)"
 			STDOUT.flush
 			confirm = STDIN.gets.chomp!
 			break if confirm.downcase == 'y'
 		end
-	
+
 		adminrole = Role.where(:name => "admin").first
 		if (adminrole == nil)
 			print "Something Is wrong with the database! (no admin role!)\n"
 			break
 		end
-		
+
 		adminuser = User.where(:username => "admin").first
 		if (adminuser == nil)
 			adminuser = User.new
@@ -37,7 +37,7 @@ namespace :swancon do
 		adminuser.password = password
 		adminuser.password_confirmation = password
 		adminuser.save
-	
+
 		if adminuser.role_symbols.index(:admin) == nil
 			adminuser.roles << adminrole
 		end
