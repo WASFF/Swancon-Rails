db8a272d8673465c5d7881f64a02b227c79fab58
Brendan Ragan <lordmortis@gmail.com>
2016-04-03 16:07:49 +0800

    Fix to search of member detail query to allow publishing to proceed

--- app/models/member_detail.rb
+++ app/models/member_detail.rb
@@ -1,20 +1,20 @@
 class MemberDetail < ActiveRecord::Base
 	belongs_to :user
-	
+
 	validates :name_first, :presence => true, :length => {:minimum => 1}
 	validates :address_1, :presence => true, :length => {:minimum => 1}
-	validates :address_3, :presence => true, :length => {:minimum => 1}	
+	validates :address_3, :presence => true, :length => {:minimum => 1}
 	validates :address_postcode, :presence => true, :length => {:minimum => 4}
-	validates :address_state, :presence => true, :length => {:minimum => 2}	
+	validates :address_state, :presence => true, :length => {:minimum => 2}
 	validates :address_country, :presence => true, :length => {:minimum => 2}
     validates :disclaimer_signed, :inclusion => {:in => [true,1] }
-		
+
 	before_save :blank_to_null
-	
+
 	def blank_to_null
 		nullarray = [:name_first, :name_last, :name_badge, :address_1, :address_2, :address_3,
 			:address_postcode, :address_state, :address_country, :phoneno]
-			
+
 		nullarray.each do |attr|
 			if self[attr] != nil
 				self[attr] = self[attr].strip
@@ -23,16 +23,16 @@ class MemberDetail < ActiveRecord::Base
 				end
 			end
 		end
-	end	
-	
+	end
+
 	def name_real
 		"#{name_first} #{name_last}"
-	end	
+	end
 
 	def email
 		user.email
 	end
-	
+
 	def name
 		if name_badge != nil
 			name_badge
@@ -40,8 +40,8 @@ class MemberDetail < ActiveRecord::Base
 			name_real
 		end
 	end
-	
+
 	def self.email_list
-		includes(:user).where(email_optin: true).where("users.email IS NOT NULL and users.email != \"\"")
-	end	
+		MemberDetail.includes(:user).where.not(users: {email: ''}).where.not(users: {email: nil}).count
+	end
 end
